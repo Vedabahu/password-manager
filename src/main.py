e4b32e02ea9f1d17010cc01b5831648c6632c21e
Vedabahu <79869936+Vedabahu@users.noreply.github.com>
2025-06-10 11:38:39 +0530

    Added password generator

--- src/main.py
+++ src/main.py
@@ -1,22 +1,30 @@
 import click
-
-from pathlib import Path
-
-
-def check_valut_existance() -> bool:
-    pass
+import secrets
+import string
 
 
-@click.command()
-def add() -> bool:
-    pass
+def main() -> None:
+    """Wrapper function for the CLI entry point."""
+    generate()
 
 
 @click.command()
-def list() -> None:
-    pass
+@click.option("--length", "-l", default=25, help="Length of the password to generate.")
+def generate(length: int) -> str:
+    """Generates a psudo random password of specified length and prints it to stdout.
+
+    Args:
+        length (int): The expected length of the generated password.
+
+    Returns:
+        str: The password.
+    """
+    alphabets = string.ascii_letters + string.digits + string.punctuation
+
+    password = "".join(secrets.choice(alphabets) for _ in range(length))
+    click.echo(password)
+    return password
 
 
-@click.command()
-def delete() -> bool:
-    pass
+if __name__ == "__main__":
+    main()
